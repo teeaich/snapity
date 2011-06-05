0ef88ef29a47bb6eebdac0c4347dc8552cf03e42
Teeaich <Teeaich@7f71471c-34aa-4ec4-836e-fef2340e14a9>
2011-06-05 09:33:49 +0000

    bugfixing: just adjusted the path more relative instead of the absolute path at the beginning
    git-svn-id: http://subversion.assembla.com/svn/snapity@7 7f71471c-34aa-4ec4-836e-fef2340e14a9

--- startapp/start/protected/extensions/webthumb/config.php
+++ startapp/start/protected/extensions/webthumb/config.php
@@ -7,6 +7,6 @@ $APIKEY = "f13780c9d3a3eb64f120f005231796a9";
 
 // check for a config file in your home dir
 $home = getenv('HOME');
-  if (file_exists("$home/yii/start/protected/extensions/webthumb/webthumb.php")) {
-  include "$home/yii/start/protected/extensions/webthumb/webthumb.php";
+  if (file_exists("$home/start/protected/extensions/webthumb/webthumb.php")) {
+  include "$home/start/protected/extensions/webthumb/webthumb.php";
 }
