41b9aef41a27fe4cd5d447e9ed35fefda761b4bd
teeaich <teeaich@gmx.de>
2011-06-08 11:56:50 +0000

    GH-4 changes for rebuild openID in model LoginForm.php

--- startapp/start/protected/models/LoginForm.php
+++ startapp/start/protected/models/LoginForm.php
@@ -40,6 +40,10 @@ class LoginForm extends CFormModel
     );
   }
 
+public function isValidURL($url)
+{
+return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
+}
   /**
    * Authenticates the password.
    * This is the 'authenticate' validator as declared in rules().
@@ -67,20 +71,19 @@ class LoginForm extends CFormModel
     if($this->_identity===null)
     {
       $this->_identity=new UserIdentityOID();
-      $this->_identity->authenticate();
-      //}
-    //if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
-    //{
-
-    //$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
-      Yii::app()->user->login($this->_identity);
-      
-
-      return true;
+      $result = $this->_identity->authenticate();
+      if ($result == false)
+      { 
+        Yii::app()->user->login($this->_identity);
+        return false;
+      } 
+      //validate the given URL from authenticate unused at the moment
+      /*if (isValidURL($result))
+      {
+          return $result;
+      }*/
+    else return $result;
     }
-    else
-      return false;
-      
   }
     
   
@@ -106,4 +109,3 @@ class LoginForm extends CFormModel
       return false;
   }
 }
-
