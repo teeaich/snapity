8023bc3686dcd38eadf1ec6be3bf6ab041bc100d
Thorsten Habig <teeaich@gmx.de>
2011-06-17 00:29:23 +0200

    GH-19 complete first redesign attempt of of the startpage. Implement animations of the big login button with toggle control. and loading the loginform with ajax. next step: either to redesign the loginform for appropiate look within html or go and get openid work again not only by the standard way but rather with a fancy dialog.

--- startapp/start/themes/minimal/views/site/index.php
+++ startapp/start/themes/minimal/views/site/index.php
@@ -1,3 +1,51 @@
+<?php Yii::app()->clientScript->registerCoreScript('jquery');?>
 <?php $this->pageTitle=Yii::app()->name; ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<div class='baseLogin'>
+  <a id='baseLoginButton_bg'><span class='baseLoginButtonText'>Login</span></a>
+
+<!--<div class='baseContent'>-->
+  <div id='withText'>with</div>
+  <div id='divForAjaxLoginForm'></div>
+<!--</div>-->
+</div>
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <script type="text/javascript">
+  
+    $(document).ready(function(){
+      
+    $(".baseLoginButtonText").toggle(function(){
+        $("#baseLoginButton_bg").animate({
+            marginLeft:'-=38%'
+        },500,'swing',function(){
+            $("#divForAjaxLoginForm").load("index.php?r=site/ajaxlogin");
+        });
+        
+    },
+    function(){
+        $("#baseLoginButton_bg").animate({
+            marginLeft: '+38%'
+        },500,'swing');
+        $("#divForAjaxLoginForm").empty();
+    });
+      
+      
+ 
+
+
+
+        
+  });
+  </script>
