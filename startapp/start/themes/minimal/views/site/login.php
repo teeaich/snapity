f305578357c20de507420931767e359775a8e4ac
Thorsten Habig <teeaich@gmx.de>
2011-06-17 00:30:17 +0200

    GH-19 a few code cleaning for the innerHtml loginform request.

--- startapp/start/themes/minimal/views/site/login.php
+++ startapp/start/themes/minimal/views/site/login.php
@@ -1,9 +1,9 @@
 <?php Yii::app()->clientscript->scriptMap['jquery.js'] = false;?>
 <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
-
-
+<div class="openIdProvider">
+    <?php echo CHtml::Link('Login with Google',CHtml::normalizeUrl(array('site/OpenId')));?>
+</div>
 <div id="dialogLoginView" class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
   'id'=>'login-form',
@@ -26,9 +26,6 @@
     <?php echo $form->labelEx($model,'password'); ?>
     <?php echo $form->passwordField($model,'password'); ?>
     <?php echo $form->error($model,'password'); ?>
-    <p class="hint">
-      Hint: You may login with <tt>demo/demo</tt> or <tt>admin/admin</tt>.
-    </p>
   </div>
 
   <div class="row rememberMe">
@@ -36,15 +33,7 @@
     <?php echo $form->label($model,'rememberMe'); ?>
     <?php echo $form->error($model,'rememberMe'); ?>
   </div>
-  <?php echo CHtml::Link('Login with Google',CHtml::normalizeUrl(array('site/OpenId')));?>
-  <?php echo CHtml::ajaxLink('Login with Google',CHtml::normalizeUrl(array('site/OpenId')),array(
-                             'dataType'=>'json',
-                             'success'=>"js:function(data) {
-                                 if (data.status == 'success') {
-                                    $('#dialogLogin').html(data.div);
-                                 }
-                              }"
-                            ),array('id'=>'send-link-'.uniqid()));?>
+  
   <div class="row buttons">
     <?php echo CHtml::ajaxSubmitButton('Login',CHtml::normalizeUrl(array('site/ajaxLogin')),array(
                              'type'=>'post',
