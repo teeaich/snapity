2519314cb9499c7ed1834463e3122aa58e7eb4d8
Thorsten Habig <teeaich@gmx.de>
2011-06-18 11:34:31 +0200

    GH-19 next step of designing, include first logo

--- startapp/start/themes/minimal/views/site/index.php
+++ startapp/start/themes/minimal/views/site/index.php
@@ -4,7 +4,7 @@
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <div class='baseLogin'>
-  <a id='baseLoginButton_bg'><span class='baseLoginButtonText'>Login</span></a>
+    <a id='baseLoginButton_bg'><span class='baseLoginButtonText'><img src='themes/minimal/images/logo1.png' alt='logo' /></span></a>
 
 <!--<div class='baseContent'>-->
   <div id='withText'>with</div>
@@ -30,6 +30,7 @@
         $("#baseLoginButton_bg").animate({
             marginLeft:'-=38%'
         },500,'swing',function(){
+            $("#divForAjaxLoginForm").css({borderLeft: '#ddd 2px solid'})
             $("#divForAjaxLoginForm").load("index.php?r=site/ajaxlogin");
         });
         
@@ -38,6 +39,7 @@
         $("#baseLoginButton_bg").animate({
             marginLeft: '+=38%'
         },500,'swing');
+        $("#divForAjaxLoginForm").css({borderLeft: ''})
         $("#divForAjaxLoginForm").empty();
     });
       
